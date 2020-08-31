1e2dcafab39b23b8fa65627fc13882b7d9d40a0a
chaseleif <chaseleif@icloud.com>
2020-08-30 23:41:21 -0500

    changed command op char from ! to \

--- hackler.py
+++ hackler.py
@@ -19,9 +19,9 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 JOKETRIGGERS = ["dumb","whatever","stupid","funny","hard","what","joke","sad","kill"]
 QUEUESIZE = 7 # how many outstanding requests to respond to
 
-class abot(commands.Bot):#discord.Client):
+class abot(commands.Bot):
     def __init__(self):
-        super().__init__(command_prefix='!')
+        super().__init__(command_prefix='\\') # catching all text ...
         self.messagequeue = deque()
         self.spamcounter=0 # incrementing counter for don't spam me messages
         self.spamflag=0 # incrementing flag for the serviceloop
@@ -79,15 +79,15 @@ class abot(commands.Bot):#discord.Client):
             onepause=False
             message = self.messagequeue.popleft()
             # system cmds
-            if message.content.startswith('!'):
-                if message.content=="!quit":
+            if message.content.startswith('\\'):
+                if message.content=="\\quit":
                     await message.channel.trigger_typing()
                     if await self.is_owner(message.author):
                         await message.channel.send(" . . . **ok** :(")
                         break
                     await asyncio.sleep(1)
                     await message.channel.send(f"ich will **doch nicht** gehen {message.author.mention}")
-                elif message.content=="!ruhe":
+                elif message.content=="\\ruhe":
                     await message.channel.trigger_typing()
                     if await self.is_owner(message.author):
                         self.besilent=True
@@ -97,7 +97,7 @@ class abot(commands.Bot):#discord.Client):
                         self.spamflag=0
                     else:
                         await message.channel.send("und **wer** bist du denn?")
-                elif message.content.startswith('!make '): # and message.content.contains(' channel '):
+                elif message.content.startswith('\\make '): # and message.content.contains(' channel '):
                     await message.channel.trigger_typing()
                     newchannel = re.sub(r'\W+', '', str(message.content[6:]).replace(' ','_'))
                     returnmessage = await chanmod.makenewchannel(theguild=message.guild, channelname=newchannel, requser=message.author)
@@ -108,7 +108,7 @@ class abot(commands.Bot):#discord.Client):
                             await message.channel.send(f"{message.author.mention}, I created the **public channel** for you")
                     else:
                         await message.channel.send(f"{message.author.mention}, **Error:** {returnmessage}")
-                elif message.content.startswith('!delete channel '):
+                elif message.content.startswith('\\delete channel '):
                     await message.channel.trigger_typing()
                     delchannel = re.sub(r'\W+', '', message.content[16:].replace(' ','_'))
                     returnmessage = await chanmod.deleteuserchannel(theguild=message.guild, channelname=delchannel, requser=message.author)
@@ -119,7 +119,7 @@ class abot(commands.Bot):#discord.Client):
                             await message.channel.send(f"{message.author.mention}, **Error:** {returnmessage}")
                     except:
                         pass
-                elif message.content.startswith('!allow '):
+                elif message.content.startswith('\\allow '):
                     await message.channel.trigger_typing()
                     userchan = re.sub(r'\W+', '', message.content[7:].replace(' ','_'))
                     returnstatus, returnmessage = await chanmod.allowuserchannel(message.guild, userchan, message.author)
@@ -127,7 +127,7 @@ class abot(commands.Bot):#discord.Client):
                         await message.channel.send(f"**All set**, {returnmessage} can now participate in your channel {message.author.mention}")
                     else:
                         await message.channel.send(f"{message.author.mention} , **Error:** {returnmessage}")
-                elif message.content.startswith('!revoke '):
+                elif message.content.startswith('\\revoke '):
                     await message.channel.trigger_typing()
                     userchan = re.sub(r'\W+', "", message.content[8:].replace(' ','_'))
                     returnstatus, returnmessage = await chanmod.revokeuserchannel(message.guild, userchan, message.author)
@@ -135,33 +135,33 @@ class abot(commands.Bot):#discord.Client):
                         await message.channel.send(f"**All set**, {returnmessage} can no longer participate in your channel {message.author.mention}")
                     else:
                         await message.channel.send(f"{message.author.mention}, **Error:** {returnmessage}")
-                elif message.content=="!mach weiter":
+                elif message.content=="\\mach weiter":
                     await message.channel.trigger_typing()
                     if await self.is_owner(message.author):
                         self.besilent=False
                         await message.channel.send(f"**jawohl** {message.author.mention} !")
                     else:
                         await message.channel.send(f"mit **wem** redest du {message.author.mention}?")
-                elif message.content=="!commands" or message.content=="!help":
+                elif message.content=="\\commands" or message.content=="\\help":
                     await message.channel.trigger_typing()
                     infomsg = "> I say hello and tell jokes.\n> \n"
-                    infomsg+= "> Commands: !word [**default**/optional] <required>\n"
+                    infomsg+= "> Commands: \\word [**default**/optional] <required>\n"
                     infomsg+= ">   __math ops supported__  - + % / *\n"
-                    infomsg+= ">   !parse <__math expr__>\n"
-                    infomsg+= ">   !solve <__math expr__>\n"
-                    infomsg+= ">   !make [**public**/private] channel <name>\n"
-                    infomsg+= ">   !delete channel <name>\n"
-                    infomsg+= ">   !allow <user> <your private channel>\n"
-                    infomsg+= ">   !revoke <user> <your private channel>"
+                    infomsg+= ">   \\parse <__math expr__>\n"
+                    infomsg+= ">   \\solve <__math expr__>\n"
+                    infomsg+= ">   \\make [**public**/private] channel <name>\n"
+                    infomsg+= ">   \\delete channel <name>\n"
+                    infomsg+= ">   \\allow <user> <your private channel>\n"
+                    infomsg+= ">   \\revoke <user> <your private channel>"
                     await message.channel.send(infomsg)
-                elif message.content.startswith('!solve'):
+                elif message.content.startswith('\\solve'):
                     await message.channel.trigger_typing()
                     mathstring, retvalue, errormsg = clpcalc.domath(message.content[6:])
                     returnstring = "That\'s easy " + message.author.mention + ", " + mathstring + " = **" + str(retvalue) + "**"
                     if errormsg!="":
                         returnstring += " (" + errormsg + ")"
                     await message.channel.send(returnstring)
-                elif message.content.startswith('!parse'):
+                elif message.content.startswith('\\parse'):
                     await message.channel.trigger_typing()
                     parsestring = message.author.mention + ", I **think** you mean " + textutils.parsemathstring(message.content[6:])
                     await message.channel.send(parsestring)
